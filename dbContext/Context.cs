cfa6eb80c79cb6e3fb26f7a9cd22feeebcf1f3dd
JohnB08 <johnb08.kodehode@gmail.com>
2024-10-05 10:23:43 +0200

    updating database source

--- dbContext/Context.cs
+++ dbContext/Context.cs
@@ -11,7 +11,7 @@ public class KTContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(
-            "Data Source=KanjiTeacherDatabase.db"
+            "Data Source=./var/data/KanjiTeacherDatabase.db"
         );
     }
 }
